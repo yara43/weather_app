0b6e743f585fa8981fcf0fbee52dcc8ffe998ea1
yara43 <yara68391@gmail.com>
2025-12-05 18:51:37 +0200

    Update api_config.dart

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -2,7 +2,6 @@ class ApiConfig {
   static const String openWeatherBaseUrl =
       'https://api.openweathermap.org/data/2.5';
 
-  // مؤقتًا حطي الـ API Key بتاع OpenWeatherMap هنا عشان التجارب
-  // بعدين لما نيجي نرفع المشروع على GitHub هنشيله من هنا
-  static const String openWeatherApiKey = '45a9ceeb18492b5abe1c8690e3de88bb';
+
+  static const String openWeatherApiKey = 'put your key';
 }
